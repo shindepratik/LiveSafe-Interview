b8827136dfdfffcaeaebbd3e67e9b5e1049ecbce
Pratik Shinde <pshinde.work@gmail.com>
2018-08-14 15:08:25 -0700

    Add news.url field to fetch and render URL

--- frontend/src/app/home/feed-card/feed-card.component.ts
+++ frontend/src/app/home/feed-card/feed-card.component.ts
@@ -6,17 +6,21 @@ import { NewsItem } from '../../core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-feed-card',
   template: `
+
     <app-card>
+
       <h4>{{ news.title }}</h4>
-
       <div class="domain">{{ news.domain }}</div>
-
       <div class="points">{{ news.points }}</div>
 
-      by <span class="author">{{ news.user }}</span>
+<! Added news.url field since it is one of the search parameters of the assignment > 
+      <div class="domain">{{ news.url }}</div>
 
+      published by <span class="author">{{ news.user }}</span>
       <span class="time-ago">{{ news.time_ago }}</span>
+    
     </app-card>
+
   `,
   styleUrls: ['./feed-card.component.scss']
 })
